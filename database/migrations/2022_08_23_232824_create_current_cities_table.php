2919454da4955d68ab09e307e1e7bba886fe9b21
gus <tavit02@hotmail.com>
2022-08-23 17:36:35 -0300

    create model and structure table

---
+++ database/migrations/2022_08_23_232824_create_current_cities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('current_cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('city')->unique()->index();
+            $table->dateTime('last_query');
+            $table->json('response');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('current_cities');
+    }
+};
